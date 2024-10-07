668ed50af910bda7c77decfb6d9d88b1d68f73df
nickgabriadze <nickgabriadze@gmail.com>
2024-10-07 22:47:06 +0400

    turn each dataframe into pivot table

---
+++ homeworks/pandas_task.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from data_example import wells_df, plates_df, experiments_df
+
+wells_pivot = wells_df.pivot_table(index=['well_id', 'well_row', 'well_column', 'plate_id'],
+                                   columns='property_name',
+                                   values='property_value',
+                                   aggfunc='first').reset_index()
+
+plates_pivot = plates_df.pivot_table(index=['plate_id', 'experiment_id'],
+                                     columns='property_name',
+                                     values='property_value',
+                                     aggfunc='first').reset_index()
+
+experiments_pivot = experiments_df.pivot_table(index='experiment_id',
+                                               columns='property_name',
+                                               values='property_value',
+                                               aggfunc='first').reset_index()
+
+merged_wells_plates = wells_pivot.merge(plates_pivot, how='left', on='plate_id')
+
+print(plates_pivot)
