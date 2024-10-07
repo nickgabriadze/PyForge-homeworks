a60b2d622b5eda1cd7763b6e04fb7b51b0b7a306
nickgabriadze <nickgabriadze@gmail.com>
2024-10-07 22:47:58 +0400

    apply join(merge) operation

--- homeworks/pandas_task.py
+++ homeworks/pandas_task.py
@@ -18,4 +18,7 @@ experiments_pivot = experiments_df.pivot_table(index='experiment_id',
 
 merged_wells_plates = wells_pivot.merge(plates_pivot, how='left', on='plate_id')
 
-print(plates_pivot)
+final_df = merged_wells_plates.merge(experiments_pivot, how='left', on='experiment_id')
+
+
+print(final_df)
